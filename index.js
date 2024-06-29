6c37fa01296bb6e8c3edb9f89e9129dbebc80fff
Mellinda wambui <mellindawambui@gmail.com>
2024-06-29 20:50:31 +0300

    First Class Function

---
+++ index.js
@@ -0,0 +1,28 @@
+function receivesAFunction(callBack) {
+   
+    callBack();
+}
+
+
+
+
+function returnsANamedFunction() {
+    function namedFunction () {
+        //function body here
+
+    }
+    return namedFunction;
+}
+
+
+
+
+
+const returnsAnAnonymousFunction = () => {
+    return function() {
+        // function body here
+    }
+}
+
+const myAnonymousFunction = returnsAnAnonymousFunction();
+myAnonymousFunction();
